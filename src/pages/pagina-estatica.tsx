166c3b4f5e9bd2dc146cfc8d7f790fb2c13871bd
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-01-27 16:50:53 -0300

    feat: turn pagina-estatica to static generation

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -6,50 +6,35 @@
  * - A página deve ser gerada no momento da build
  * - A página deve ser atualizada a cada 1 minuto
  */
+import styles from "@/styles/lista.module.css";
+import { ICity } from "@/types/city.d";
 
-import { useEffect, useState } from 'react';
+export default function Lista({ cities }: { cities: ICity[] }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2>Lista de cidades</h2>
 
-import styles from '@/styles/lista.module.css';
-import { ICity } from '@/types/city.d';
+        <div data-list-container>
+          {cities.map((city) => (
+            <div data-list-item key={city.id}>
+              {city.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default function Lista() {
-	const [list, setUsers] = useState<Array<ICity>>([
-		{
-			id: new Date().getTime().toString(),
-			name: 'São Paulo',
-		},
-	]);
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:8080/api/cities/10");
+  const cities = await res.json();
 
-	async function getList() {
-		try {
-			const response = await fetch('/api/cities/10');
-			const data = await response.json();
-
-			if (!response.ok) throw new Error('Erro ao obter os dados');
-
-			setUsers(data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
-
-	useEffect(() => {
-		getList();
-	}, []);
-
-	return (
-		<div className={styles.container}>
-			<div className={styles.content}>
-				<h2>Lista de cidades</h2>
-
-				<div data-list-container>
-					{list.map((city) => (
-						<div data-list-item key={city.id}>
-							{city.name}
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return {
+    props: {
+      cities,
+    },
+    revalidate: 60,
+  };
 }
